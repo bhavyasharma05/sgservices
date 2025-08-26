10e321f29c09354278d0134eb6306ffd397503d3
bhavya kumar sharma <bhavyakumarsharma1234@gmail.com>
2025-08-26 22:03:05 +0530

    Updated app.tsx

---
+++ sgpsi-main/src/app.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello React + Vite + TypeScript 🚀</h1>
+      <p>If you see this, your setup works!</p>
+    </div>
+  );
+}
